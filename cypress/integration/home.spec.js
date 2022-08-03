06f04ca6cbf5b5efc3eacba8f33e7be0b6f7210a
Sae <winny9641@gmail.com>
2022-08-03 19:49:47 -0400

    add hompage cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe("Jungle app", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should show products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+ \
+});
